00d04fa741159031d61cec71cd6888f68ad1436d
CserhiBerni <59263479+CserhiBerni@users.noreply.github.com>
2022-05-15 22:37:52 +0200

    finished the not found page.

--- app-frontend/src/components/404/NotFound.js
+++ app-frontend/src/components/404/NotFound.js
@@ -1,7 +1,15 @@
+import './NotFound.css';
 import React from "react";
 
 function NotFound() {
-    return (<h1 style={{color: 'white'}}>404</h1>);
+    return (
+        <div className="not-found">
+            <h1>Error 404 :(</h1>
+            <h4>Sajnos az oldalnak ilyen alcíme nem létezik.<br /> Valószínűleg elírtál valamit, <br />ha pedig
+                biztos vagy benne, hogy nem, <br />akkor kérlek jelezz nekünk.
+            </h4>
+        </div>
+    );
 }
 
 export default NotFound;
