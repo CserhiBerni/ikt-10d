e233b6775ab81b3a1efa847a5380e780d65ada04
CserhiBerni <59263479+CserhiBerni@users.noreply.github.com>
2022-05-17 19:57:55 +0200

    done the portfolios.

--- app-frontend/src/components/bernat/Bernat.js
+++ app-frontend/src/components/bernat/Bernat.js
@@ -9,8 +9,9 @@ function Bernat() {
                     <h1>Magamról</h1>
                 </div>
                 <div className="item-content">
-                    <p>lorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cum
-                        lorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cum
+                    <p>Gáspár Bernát vagyok, 16 éves tanuló. Jelenleg a Petrik Lajos technikumba járok be minden nap,
+                        általánosba a zuglói Herman Ottó Általánosba jártam. Nem a kedvenc eltöltésem iskolába
+                        járni, viszont informatikát tanulok és igyekszem <br />a maximumot kihozni belőle.
                     </p>
                 </div>
             </div>
@@ -19,8 +20,10 @@ function Bernat() {
                     <h1>Tapasztalat</h1>
                 </div>
                 <div className="item-content">
-                    <p>lorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cum
-                        lorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cum
+                    <p>Nagyon nagy projektekben koromhoz fogva még nem vettem részt, viszont barátaimmal
+                        számos érdekes dolgot próbáltam ki az informatika, de főleg a programozás terén.
+                        Remélhetőleg hamarosan viszont már való élet beli tapasztalatot is gyűjthetek.
+
                     </p>
                 </div>
             </div>
@@ -29,8 +32,9 @@ function Bernat() {
                     <h1>Kedvenc tantárgyak</h1>
                 </div>
                 <div className="item-content">
-                    <p>lorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cum
-                        lorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cum
+                    <p>Legfőképpen azt szeretem, amikor nincsnek órák és tanulhatok magamtól, viszont ha mondanom kéne párat,
+                        akkor igazából minden reál tantárgyat mondanék. Ezekben van nagyobb ismeretem, részben
+                        ezért is szeretem ezeket jobban.
                     </p>
                 </div>
             </div>
@@ -39,8 +43,9 @@ function Bernat() {
                     <h1>Jövőkép</h1>
                 </div>
                 <div className="item-content">
-                    <p>lorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cum
-                        lorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cum
+                    <p>Ha kijártam végre valahára az iskolát utána már rendeltetés szerűen programozó szeretnék
+                        lenni, saját vállalkozásokba kezdeni, megismerni az üzletet. Azt, hogy később még
+                        hol akarok tanulni, arról fogalmam sincs egyenlőre.
                     </p>
                 </div>
             </div>
