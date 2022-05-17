037e14f119e1d320523dbc68c5a4e6fcfd73a95a
CserhiBerni <59263479+CserhiBerni@users.noreply.github.com>
2022-05-17 19:06:11 +0200

    working on styling.

--- app-frontend/src/components/bernat/Bernat.js
+++ app-frontend/src/components/bernat/Bernat.js
@@ -4,7 +4,46 @@ import './Bernat.css';
 function Bernat() {
     return (
         <div className="main">
-            
+            <div className="list-item">
+                <div className="item-title">
+                    <h1>Magamról</h1>
+                </div>
+                <div className="item-content">
+                    <p>lorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cum
+                        lorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cum
+                    </p>
+                </div>
+            </div>
+            <div className="list-item">
+                <div className="item-title">
+                    <h1>Tapasztalat</h1>
+                </div>
+                <div className="item-content">
+                    <p>lorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cum
+                        lorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cum
+                    </p>
+                </div>
+            </div>
+            <div className="list-item">
+                <div className="item-title">
+                    <h1>Kedvenc tantárgyak</h1>
+                </div>
+                <div className="item-content">
+                    <p>lorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cum
+                        lorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cum
+                    </p>
+                </div>
+            </div>
+            <div className="list-item">
+                <div className="item-title">
+                    <h1>Jövőkép</h1>
+                </div>
+                <div className="item-content">
+                    <p>lorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cum
+                        lorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cumlorem ipsum cum
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
